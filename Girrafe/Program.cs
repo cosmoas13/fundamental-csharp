8d65e9ed8bbda552b410a806ba4a784a8dc5e8eb
cosmoas13 <cosmoas13@gmail.com>
2022-09-26 18:09:05 +0700

    variables on c#

--- Girrafe/Program.cs
+++ Girrafe/Program.cs
@@ -10,10 +10,11 @@ namespace Girrafe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            string characterName = "Richard Antonio Yahaha";
+            int characterAge;
+            characterAge = 27;
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
 
             Console.ReadLine();
         }
