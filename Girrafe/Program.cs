ab70928bd72194bb039dba1aa37d919354ba40b0
cosmoas13 <cosmoas13@gmail.com>
2022-09-27 18:37:13 +0700

    create simple user input

--- Girrafe/Program.cs
+++ Girrafe/Program.cs
@@ -10,14 +10,14 @@ namespace Girrafe
     {
         static void Main(string[] args)
         {
-            string phrase = "fundamental C#";
-            char grade = 'A';
-            int age = 27;
-            double gpa = 3.14;
-            bool isMale = true;
-
-            Console.WriteLine("Hello this is " + phrase);
-            Console.WriteLine(gpa);
+            string name, age;
+            // Writes the text representation
+            // of the specified value or values to the standard output stream.
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " your age is " + age);
 
             Console.ReadLine();
 
