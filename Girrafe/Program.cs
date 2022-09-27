0063e9b2d26221e7709fad0cd1b6d8f078e6bde7
cosmoas13 <cosmoas13@gmail.com>
2022-09-27 18:20:22 +0700

    working with strings

--- Girrafe/Program.cs
+++ Girrafe/Program.cs
@@ -10,17 +10,29 @@ namespace Girrafe
     {
         static void Main(string[] args)
         {
-            string phrase = "fundamental C#";
-            char grade = 'A';
-            int age = 27;
-            double gpa = 3.14;
-            bool isMale = true;
-
-            Console.WriteLine("Hello this is " + phrase);
-            Console.WriteLine(gpa);
-
+            string phrase;
+            phrase = "Fundamental C#";
+            // \n for new line
+            Console.WriteLine("My Name is Arthur\nand My name is Richard");
+            // \" for add double quote
+            Console.WriteLine("Rose is \"Red\"" + " and violet is \'purple\'");
+            // \t for horizontal tab
+            Console.WriteLine("Link\tMove");
+            // for more info can see on learn.microsoft dotnet/csharp
+            // how get length on string
+            Console.WriteLine(phrase.Length);
+            // make string to Caps
+            Console.WriteLine(phrase.ToUpper());
+            // and this for lower string
+            Console.WriteLine(phrase.ToLower());
+            // using contains, return true, because C# it does.
+            Console.WriteLine(phrase.Contains("C#"));
+            // using indexOf to tell us what index position C# start inside, return 12
+            Console.WriteLine(phrase.IndexOf("C#"));
+            // get string using index position, and length,
+            // return C cause we start from index 12 and just put 1 length
+            Console.WriteLine(phrase.Substring(12, 1));
             Console.ReadLine();
-
         }
     }
 }
