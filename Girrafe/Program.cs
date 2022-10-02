eb585c1d5d4b437f1e68c03ef4fce76b681146e6
cosmoas13 <cosmoas13@gmail.com>
2022-10-02 12:13:07 +0700

    day name

--- Girrafe/Program.cs
+++ Girrafe/Program.cs
@@ -10,32 +10,42 @@ namespace Girrafe
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the Number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter the Operator: ");
-            string op = Console.ReadLine();
-
-            Console.Write("Enter the Number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            if(op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            } else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            } else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            } else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            } else
-            {
-                Console.WriteLine("Invalid Operator");
-            }
+            Console.WriteLine(GetDay(53));
             Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 8:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            return dayName;
         }
     }
 }
